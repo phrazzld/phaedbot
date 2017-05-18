205df4192b4a5c42074e6bc7683fe8e58d49eaaf
Phaedrus <phraznikov@gmail.com>
2017-05-18 15:45:43 -0700

    Fix user message style inconsistency

--- public/js/smarts.js
+++ public/js/smarts.js
@@ -36,13 +36,35 @@ var processRequest = function (query) {
   })
 }
 
+var writeUserMessage = function (query) {
+    var requestDiv = document.createElement("div")
+    requestDiv.className = "request row"
+    var authorDiv = document.createElement("div")
+    authorDiv.className = "col-xs-3 col-sm-2 col-sm-offset-3 col-md-1 col-md-offset-4"
+    var authorSpan = document.createElement("span")
+    authorSpan.className = "author"
+    var author = document.createTextNode("You")
+    authorSpan.appendChild(author)
+    authorDiv.appendChild(authorSpan)
+    var msgDiv = document.createElement("div")
+    msgDiv.className = "col-xs-9 col-sm-6 col-md-5"
+    var msgP = document.createElement("p")
+    msgP.className = "message"
+    var msg = document.createTextNode(query)
+    msgP.appendChild(msg)
+    msgDiv.appendChild(msgP)
+    requestDiv.appendChild(authorDiv)
+    requestDiv.appendChild(msgDiv)
+    $("#chat").append(requestDiv)
+    $("#message-contents").val("")
+}
+
+
 $(document).ready(function () {
   $("#message-form").submit(function (e) {
     e.preventDefault()
     var query = $("#message-contents").val()
-    var userMsg = "<div class='request row'><div class='col-xs-3 col-sm-2 col-sm    -offset-3 col-md-1 col-md-offset-4'><span class='author'>You</span></div><div class='    col-xs-9 col-sm-6 col-md-5'><p class='message'>" + query + "</p></div></div>"
-    $("#chat").append(userMsg)
-    $("#message-contents").val("")
+    writeUserMessage(query)
     processRequest(query)
   })
 })
